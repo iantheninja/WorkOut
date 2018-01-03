265c1cd5d3f22e9352f786484d23f51b60187b66
Ian Munene <munene.ian@live.com>
2018-01-03 18:28:53 +0300

    workout list and detail fragments added. detail fragment ui added for differentiation

--- app/src/main/java/com/ianmunene/workout/MainActivity.java
+++ app/src/main/java/com/ianmunene/workout/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ianmunene.workout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onShowDetails(View view) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
     }
 }
