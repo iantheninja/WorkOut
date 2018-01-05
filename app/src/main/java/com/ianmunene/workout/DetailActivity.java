ab0df2ce553e8c2bb712bd1a87c24c136d6791e8
Ian Munene <munene.ian@live.com>
2018-01-05 19:21:50 +0300

    fragment pulling data from workout class and updating the views through the fragments onCreate

--- app/src/main/java/com/ianmunene/workout/DetailActivity.java
+++ app/src/main/java/com/ianmunene/workout/DetailActivity.java
@@ -9,5 +9,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        WorkoutDetailFragment fragment = (WorkoutDetailFragment)
+                getSupportFragmentManager().findFragmentById(R.id.frag_detail);
+        fragment.setWorkout(1);
     }
 }
